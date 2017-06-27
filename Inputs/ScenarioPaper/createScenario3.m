e8bb90e02414e5dad73111cfbee0da27e023927e
Sergio <gsergio@chalmers.se>
2017-06-27 17:59:49 +0200

    two new scenarios

---
+++ Inputs/ScenarioPaper/createScenario3.m
@@ -0,0 +1,28 @@
+%inputs
+close all;
+% is an array that contains the mapping between the integer and the
+% services. Position i contains the name of the service associated with the
+% integer i
+global actions;
+actions={'recharge', 'r1loadbox1', 'r2loadbox1', 'r2unloadbox1', 'detectunloadingbox1', 'takeApicture'};
+
+
+% contains the map of the environment
+environment=EnvironmentMap();
+
+%grid = ones(sergio.x*scale+1,sergio.y*scale+1)*whitevalue;
+grid = ones(environment.x*scale+1,environment.y*scale+1)*whitevalue;
+hh=figure;
+grid=visualizeGrid(grid, environment);
+figure(); imshow(grid, c);
+xlabel('x'); ylabel('y');
+
+%sys(1)=Robot1(environment.map, environment.pmap);
+%sys(2)=Robot2(environment.map, environment.pmap);
+%sys(3)=Robot3(environment.map, environment.pmap);
+
+%spec(1)=ExistenceGoal(1, 2, 1, 1);
+%spec(1)=ExistenceGoal(1, 2, 1, [1,2]);
+%spec(2)=ResponseGoal(3, 4, [1, 2], [1,3]);
+%spec(3)=ResponseGoal(5, 6, [2,3], 3);
+
