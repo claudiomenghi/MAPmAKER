af4895956be63cb19ffa5afce7a8e6e1ef1aa202
Sergio <gsergio@chalmers.se>
2017-10-23 14:52:23 +0200

    add new launching function

---
+++ MAPmAKER/mapmaker_exp.m
@@ -0,0 +1,10 @@
+function mapmaker_exp(RQ, example, experiment)
+
+    path='~/MATLAB/MAPmAKER/Evaluation/RQ1/Example1/Experiment2'
+    addpath(sprintf(path));
+%     addpath('~/MATLAB/MAPmAKER/Evaluation/RQ1/Example1/Experiment2');
+%     addpath('~/MATLAB/MAPmAKER/Evaluation/RQ1/Example1/Algorithms/');
+%     addpath('~/MATLAB/MAPmAKER/ReplicationPackage/RQ1/Example1/Experiment2/');
+     experiment2;
+
+end
